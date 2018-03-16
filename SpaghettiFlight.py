074b91e30882d0ad4404f548a208c40ae413f918
Eric Karschner <erickarschner@gmail.com>
2018-03-16 10:14:39 -0400

    New Movement method

--- SpaghettiFlight.py
+++ SpaghettiFlight.py
@@ -7,26 +7,32 @@ drone = libardrone.ARDrone();
 while(True):
     if joy.A():
         drone.takeoff();
-        print("A button pressed!")
+    #     print("A button pressed!")
     if joy.B():
         drone.land();
     if joy.Y():
         drone.hover();
-    if joy.leftX() < 0:
-        drone.move_left();
-    if joy.leftX() > 0:
-        drone.move_right();
-    if joy.leftY() < 0:
-        drone.move_backward();
-    if joy.leftY() > 0:
-        drone.move_forward();
-    if joy.rightX() < 0:
-        drone.turn_left();
-    if joy.rightX() > 0:
-        drone.turn_right();
-    if joy.rightY() < 0:
-        drone.move_down();
-    if joy.rightY() > 0:
-        drone.move_up();
-    if joy.Guide() and joy.Start():
-        drone.reset();
+
+    drone.move(joy.leftX(), joy.leftY(), joy.rightTrigger() - joy.leftTrigger(), joy.rightX());
+
+    # Legacy code: do not remove until the new method is tested and usable
+
+    
+    # if joy.leftX() < 0:
+    #     drone.move_left();
+    # if joy.leftX() > 0:
+    #     drone.move_right();
+    # if joy.leftY() < 0:
+    #     drone.move_backward();
+    # if joy.leftY() > 0:
+    #     drone.move_forward();
+    # if joy.rightX() < 0:
+    #     drone.turn_left();
+    # if joy.rightX() > 0:
+    #     drone.turn_right();
+    # if joy.rightY() < 0:
+    #     drone.move_down();
+    # if joy.rightY() > 0:
+    #     drone.move_up();
+    # if joy.Guide() and joy.Start():
+    #     drone.reset();
